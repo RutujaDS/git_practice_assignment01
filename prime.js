58a7b9da208d40db58b57a44e7a13513f25a20eb
Rutuja Shinde <shindedrutuja@gmail.com>
2022-12-08 19:53:00 +0530

    codedone

--- prime.js
+++ prime.js
@@ -1,2 +1,15 @@
-funtion checkprime(num)
-let bag="";
+let number=13;
+funtion checkprime(number){
+	
+	count = 0;
+	for(let i=0; i<number;i++){
+		if(number%i==0){
+		count++
+		}
+	if(count == 2){
+		console.log("Prime");
+	}else{
+		console.log("not Prime")
+	}
+	}
+	checkprime(13);
